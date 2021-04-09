336358971403b89ba38921ecdd94322cfd15bfdb
ubuntupbc <646047990@qq.com>
2021-04-09 11:17:30 +0800

    lab-win

--- getBertWordvec.py
+++ getBertWordvec.py
@@ -3,7 +3,16 @@ from pytorch_transformers import BertTokenizer, BertModel
 
 tokenizer = BertTokenizer.from_pretrained('E:/bert/6-new-12w-0')
 model = BertModel.from_pretrained('E:/bert/6-new-12w-0')
-input_ids = torch.tensor(tokenizer.encode("自然语" * 1000)).unsqueeze(0)  # Batch size 1
+input_ids = torch.tensor(tokenizer.encode("自然语言处理")).unsqueeze(0) # Batch size 1
+# input_ids = torch.tensor(
+#     [tokenizer.encode("ATGTTT"), tokenizer.encode("ATGAGC"), tokenizer.encode("ATGGAT")])  # Batch size 3, Seq 1
+
+input_ids = torch.tensor(tokenizer.encode("ATGTTTATGAGC")).unsqueeze(0)  # Batch size 1
+
+input_ids = torch.tensor(
+    [tokenizer.encode("自然语言处理"), tokenizer.encode("自然语言处理")])  # Batch size 3, Seq 7
+
+print(input_ids)
 outputs = model(input_ids)
 # last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
 sequence_output = outputs[0]
