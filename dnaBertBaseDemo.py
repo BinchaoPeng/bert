443845f70c409c4e3c247535593292f1181460da
ubuntupbc <646047990@qq.com>
2021-05-01 13:23:28 +0800

    lab-ubuntu:feature longformer

--- dnaBertBaseDemo.py
+++ dnaBertBaseDemo.py
@@ -1,11 +1,20 @@
 import torch
-from pytorch_transformers import BertTokenizer, BertModel
-import platform
+from transformers import BertModel, BertConfig, DNATokenizer
 
 model_name = 'pre-model/' + '6-new-12w-0'
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertModel.from_pretrained(model_name)
 
-sep = tokenizer.tokenize("ATGC")
+config = BertConfig.from_pretrained(model_name)
+tokenizer = DNATokenizer.from_pretrained(model_name)
+model = BertModel.from_pretrained(model_name, config=config)
 
-print(sep)
+sequence = "AATCTA ATCTAG TCTAGC CTAGCA"
+model_input = tokenizer.encode_plus(sequence, add_special_tokens=True, max_length=512)["input_ids"]
+model_input = torch.tensor(model_input, dtype=torch.long)
+model_input = model_input.unsqueeze(0)  # to generate a fake batch with batch size one
+
+output = model(model_input)
+print(output[0])
+print(output[0].shape, "\n\n")
+# CLS token's embedding
+print(output[1])
+print(output[1].shape, "\n\n")
